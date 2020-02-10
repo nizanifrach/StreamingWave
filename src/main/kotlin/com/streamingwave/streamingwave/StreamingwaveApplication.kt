eddc412be9a1bc730b7f50530fcae8cf9123e559
nizanifrach <nizanifrach@gmail.com>
2020-02-10 20:48:31 +0200

    first commit

---
+++ src/main/kotlin/com/streamingwave/streamingwave/StreamingwaveApplication.kt
@@ -0,0 +1,11 @@
+package com.streamingwave.streamingwave
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StreamingwaveApplication
+
+fun main(args: Array<String>) {
+	runApplication<StreamingwaveApplication>(*args)
+}
---
+++ src/test/kotlin/com/streamingwave/streamingwave/StreamingwaveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.streamingwave.streamingwave
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StreamingwaveApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
